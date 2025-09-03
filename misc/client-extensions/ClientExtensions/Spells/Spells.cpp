662605b82f7ddeb15ed265b2ecdb9ae82cbf143a
Saty <5752427+SatyPardus@users.noreply.github.com>
2025-09-03 10:14:08 +0200

    Spell math fixes

--- misc/client-extensions/ClientExtensions/Spells/Spells.cpp
+++ misc/client-extensions/ClientExtensions/Spells/Spells.cpp
@@ -142,10 +142,10 @@ CLIENT_DETOUR_THISCALL(GetLineSegment, 0x004F6450, int, (float a2, float a3, C3V
             if (targetingSpell)
             {
                 auto data     = &targetingSpell->data;
-                float minDist = 0.0f, maxDist = 0.0f;
-                Spell_C::GetSpellRange(activeObjectPtr, data->spellId, &minDist, &maxDist, 0);
+                float minSpellDist = 0.0f, maxSpellDist = 0.0f;
+                Spell_C::GetSpellRange(activeObjectPtr, data->spellId, &minSpellDist, &maxSpellDist, 0);
 
-                float left = 0.0f, right = maxDist * 3;
+                float left = 0.0f, right = maxSpellDist * 3;
                 float bestDist = -1.0f;
                 C3Vector bestPoint, bestPos;
 
@@ -176,15 +176,14 @@ CLIENT_DETOUR_THISCALL(GetLineSegment, 0x004F6450, int, (float a2, float a3, C3V
                     start = {start.x + dir.x * t, start.y + dir.y * t, start.z + dir.z * t};
                 }
 
-
+                
+                float minDist = maxSpellDist * Spells::g_spell_min_clip_distance_percentage_cvar->m_numberValue;
                 while (right - left > 0.5f)
                 {
                     float mid = (left + right) * 0.5f;
                     point     = GetPointAtDistance(start, end, mid);
                     pos       = {point.x, point.y, point.z - 500.0f};
                     distance  = 1.0f;
-
-                    float minDist = maxDist * Spells::g_spell_min_clip_distance_percentage_cvar->m_numberValue;
 
                     if (TraceLine(&point, &pos, &hitpoint, &distance, 0x10111, 0))
                     {
